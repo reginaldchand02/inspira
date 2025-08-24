5f40cb255b687fae3e82e8ff9dc53dd3ce705f60
Reginald Sahil Chand <reginaldchand02@gmail.com>
2025-08-24 15:13:46 +1200

    chore: update logo-brand stroke width from 2 to 5

--- components/logo-brand.tsx
+++ components/logo-brand.tsx
@@ -3,7 +3,7 @@ export function LogoBrand() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 256"
-      strokeWidth={2}
+      strokeWidth={5}
       strokeLinecap="round"
       strokeLinejoin="round"
     >
