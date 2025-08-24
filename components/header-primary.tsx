2c992aeff710a7e26e2a62c0df39591117c6c2f1
Reginald Sahil Chand <reginaldchand02@gmail.com>
2025-08-24 15:43:11 +1200

    feat: create header-primary

---
+++ components/header-primary.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { LogoBrand } from "./logo-brand";
+
+export function HeaderPrimary() {
+  return (
+    <header className="max-w-7xl w-[90%] sm:w-[70%] md:w-1/2 mx-auto h-16 p-5 pl-2 fixed inset-0 top-5 flex items-center justify-between backdrop-blur-md bg-background/40 rounded-full z-50">
+      {/* Logo starts here */}
+      <Link href={"/"} className="flex items-center gap-2.5 group">
+        <div className="w-12 h-12">
+          <LogoBrand />
+        </div>
+
+        <span className="bg-gradient-to-r from-[var(--inspira-design-primary)] via-[var(--inspira-development-secondary)] to-[var(--inspira-ai-accent)] bg-clip-text text-transparent group-hover:from-[var(--inspira-ai-accent)] group-hover:via-[var(--inspira-development-secondary)] group-hover:to-[var(--inspira-design-primary)] transition-all">
+          Inspira
+        </span>
+      </Link>
+      {/* Logo ends here */}
+
+      {/* Navigation starts here */}
+      <Link
+        href={"/"}
+        className="font-light hover:text-inspira-spark-highlight transition-colors"
+      >
+        Home
+      </Link>
+      {/* Navigation ends here */}
+    </header>
+  );
+}
