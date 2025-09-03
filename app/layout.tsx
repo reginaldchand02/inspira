833d9fb15b09f8099a2027e2994f27b677c1a0a2
Reginald Sahil Chand <reginaldchand02@gmail.com>
2025-09-03 20:04:02 +1200

    feat: enable support for light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,8 +74,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           <HeaderPrimary />
