52a1d23b39ff4a77bb77b2370a3e9c9fad8511b0
Reginald Sahil Chand <reginaldchand02@gmail.com>
2025-09-04 21:29:43 +1200

    chore: add use client directive

--- components/typography-h2.tsx
+++ components/typography-h2.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export function TypographyH2({ children }: { children: React.ReactNode }) {
   return (
     <h2 className="text-center text-3xl font-semibold tracking-tight text-balance">
